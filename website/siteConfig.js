a82e85d836757ed00f38c61adbea4a9205e52dc6
Tina Aggarwal <tina.14a@gmail.com>
2020-10-02 12:30:28 -0700

    Updated website url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -15,7 +15,7 @@ const siteConfig = {
     title: 'Tina Aggarwal', // Title for your website.
     tagline: 'Software Development Engineer',
     profilePhoto: 'img/profile.png',
-    url: 'https://github.com/tinaaggarwal', // Your website URL
+    url: 'https://tinaaggarwal.github.io/', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
